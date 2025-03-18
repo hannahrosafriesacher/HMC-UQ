remove bug in autocorr computation

hmc_uq/train_hmc.py
@@ -16,7 +16,7 @@ from utils.evaluation import HMCPredictivePerformance, HMCSampleEvaluation
 from utils.data import SparseDataset
 import wandb
 
-import cProfile
+from timeit import default_timer as timer
 
 args = get_args(config_file = 'configs/models/hmc.yaml')
 print("Loaded Configuration:")
@@ -124,7 +124,7 @@ for chain in range(nr_chains):
     #TODO: check code what happens if I specify only 1 tau?
     tau_list = [tau]
     tau_list = torch.tensor(tau_list).to(device)
-
+    start = timer()
     params_gpu, accept_rate = hamiltorch.sample_model(
         net, 
         x = train_dataset.__getdatasets__()[0], 
@@ -138,6 +138,8 @@ for chain in range(nr_chains):
         model_loss=model_loss,
         debug = 2
         )
+    end = timer()
+    print('HMC Sampler', end - start)
     logs.update({f'ar/chain{chain + 1}': accept_rate})
 
     params = torch.stack(params_gpu, dim = 0).cpu().numpy()
@@ -171,25 +173,60 @@ logs.update({f'/val/auc/average': np.mean(auc_val)})
 
 if evaluate_samples:
     sample_eval = HMCSampleEvaluation(params_chains)
-    sample_eval.calculate_autocorrelation()
+
+    start = timer()
+    ac = sample_eval.calculate_autocorrelation()
+    end = timer()
+    print('AUTOCORR', end - start)
+
+    start = timer()
     logs.update({f'SplitRhat': sample_eval.split_rhat(burnin=0, rank_normalized=False).mean()})
+    end = timer()
+    print('SplitRhat', end - start)
+
+    start = timer()
     logs.update({f'rnSplitRhat': sample_eval.split_rhat(burnin=0, rank_normalized=True).mean()}) #Convergence
+    end = timer()
+    print('rnSplitRhat', end - start)
+
+    start = timer()
     logs.update({f'GEW/chain{chain +1}': gew for chain, gew in enumerate(sample_eval.geweke())}) #Convergence
+    end = timer()
+    print('AGEW', end - start)
+
+    start = timer()
     logs.update({f'IPS/chain{chain +1}': ips for chain, ips in enumerate(sample_eval.ips_per_chain(burnin=False))}) #SampleSize
+    end = timer()
+    print('IPS', end - start)
+
+    start = timer()
     logs.update({f'IPS-burnin/chain{chain +1}': ips for chain, ips in enumerate(sample_eval.ips_per_chain(burnin=True))})
+    end = timer()
+    print('IPS-burnin', end - start)
+
+    start = timer()
     split_rhat_az, rnsplit_rhat_az = sample_eval.rhat_az()
     logs.update({f'SplitRhat/AZ': split_rhat_az.mean().item()})
-    logs.update({f'rnSplitRhat/AZ': rnsplit_rhat_az.mean().item()})
+    #logs.update({f'rnSplitRhat/AZ': rnsplit_rhat_az.mean().item()})
+    end = timer()
+    print('SplitRhat/AZ', end - start)
+
+    start = timer()
     logs.update({f'IPS/AZ': sample_eval.ess_az().mean().item()})
+    end = timer()
+    print('IPS/AZ', end - start)
     #TODO: Log Acceptance rate, IPS for all chains combined
 
- 
+    start = timer()
     #Shift WANDB in Evaluation file?
     wandb.log({f'Rhat vs Burn-in': sample_eval.rhat_burnin_plot()})
+    wandb.log({f'IPS vs Burn-in': sample_eval.ips_burnin_plot()})
     wandb.log({f'Autocorrelation': sample_eval.autocorrelation_plot()})
     trace_plots = sample_eval.trace_plot(net.state_dict())
     for plot in trace_plots:
         wandb.log({f'{plot}': trace_plots[plot]})
+    end = timer()
+    print('Plots', end - start)
 
 
 if evaluate_testset:
@@ -238,4 +275,5 @@ if save_model:
             yaml.dump(lookup, file)
 
 wandb.log(logs)    
+
 

hmc_uq/utils/evaluation.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.stats import pearsonr, rankdata, norm
+from scipy.signal import correlate
 import torch
 import torch.nn.functional as F
 from sklearn.metrics import roc_auc_score
@@ -92,20 +93,40 @@ class HMCSampleEvaluation:
         self.IPS = {'#Burn-in' : [], 'Chain': [], 'IPS':[]}
         self.RHAT = {'#Burn-in' : [], 'Split-Rhat':[], 'rnSplit-Rhat':[]}
 
+    
+    def autocorrelation_singleparam(self, params_chain):
+        #to get normalized correlation
+            params_chain_a = (params_chain- np.mean(params_chain)) / (np.std(params_chain) * len(params_chain))
+            params_chain_b = (params_chain - np.mean(params_chain)) / np.std(params_chain)
+            ac_param = correlate(params_chain_a, params_chain_b)
+            half_idx = len(ac_param)//2
+            ac_param = ac_param[half_idx:(half_idx + self.max_lag)]
+            return ac_param
+
     def calculate_autocorrelation(self):
         for nrbisamples in [0, int(self.max_burnin), 10]: #TODO
             for chain in range(self.nr_chains):
                 burnin = False if nrbisamples ==0 else True 
                 params_chain = self.params_chains[chain][nrbisamples:]
-                nr_samples = self.nr_samples - nrbisamples
-                max_lag = math.floor((self.nr_samples - nrbisamples)/2) if nr_samples <+ 200 else 100
-                for lag in range(0, max_lag):
-                    ac = np.mean(pearsonr(params_chain[lag:], params_chain[:(nr_samples-lag)], axis = 0)[0])
-                    self.AC['Chain'].append(chain)
-                    self.AC['LAG'].append(lag)
-                    self.AC['AUTOCORR'].append(ac)
-                    self.AC['#Burn-in'].append(nrbisamples)
-                    self.AC['Burn-in'].append(burnin)
+                nr_samples_bi = self.nr_samples - nrbisamples
+                self.max_lag = nr_samples_bi//2 if nr_samples_bi <= 400 else 200
+
+                ac = np.apply_along_axis(self.autocorrelation_singleparam, arr = params_chain, axis = 0)
+                ac_mean = np.mean(ac, axis = 1)
+                            
+                self.AC['Chain'].extend([chain] * self.max_lag)
+                self.AC['LAG'].extend(range(self.max_lag))
+                self.AC['AUTOCORR'].extend(ac_mean)
+                self.AC['#Burn-in'].extend([nrbisamples] * self.max_lag)
+                self.AC['Burn-in'].extend([burnin] * self.max_lag)
+
+                #for lag in range(0, max_lag):
+                #    ac = np.mean(pearsonr(params_chain[lag:], params_chain[:(nr_samples-lag)], axis = 0)[0])
+                #    self.AC['Chain'].append(chain)
+                #    self.AC['LAG'].append(lag)
+                #    self.AC['AUTOCORR'].append(ac)
+                #    self.AC['#Burn-in'].append(nrbisamples)
+                #    self.AC['Burn-in'].append(burnin)
         self.AC = pd.DataFrame(self.AC)
 
     
@@ -186,7 +207,7 @@ class HMCSampleEvaluation:
         df_burnin = self.AC[self.AC['#Burn-in'] == burnin]
         for chain in range(self.nr_chains):
             autocorr = 0
-            max_lag = math.floor((self.nr_samples - burnin)/2)
+            max_lag = (self.nr_samples - burnin)//2
             df_chain = df_burnin[df_burnin['Chain'] == chain]
             for lag in range(0, max_lag, 2):
                 ips = 1
@@ -219,16 +240,16 @@ class HMCSampleEvaluation:
         return rhat_burnin
     
     
-    '''
+    
     def ips_burnin_plot(self):
         plt.cla()
         for burnin in range(0, self.max_burnin, self.burnin_step):
             for chain in range(self.nr_chains):
-                self.IPS['Burn-in'].append(burnin)
+                self.IPS['#Burn-in'].append(burnin)
                 self.IPS['IPS'].append(self.ips_per_chain(burnin)[chain])
                 self.IPS['Chain'] = chain
-        ips_burnin = sns.lineplot(data = self.IPS, x= 'Burn-in', y = 'IPS', hue = 'Chain')
-        return ips_burnin'''
+        ips_burnin = sns.lineplot(data = self.IPS, x= '#Burn-in', y = 'IPS', hue = 'Chain')
+        return ips_burnin
     
     #def autocorrelation_plot:
     def autocorrelation_plot(self):